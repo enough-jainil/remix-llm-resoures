0a0382e5a263a3f4626cfc2b7a138d854fe1f531
Jainil Prajapati 🪐 <github-jainil@1209.in>
2024-12-18 17:38:34 +0530

    Update robots.txt files to streamline crawler directives
    - Modified the dynamic `robots.txt` route to simplify the user-agent and allow directives, while ensuring the sitemap location is clearly specified.
    - Updated the static `robots.txt` file in the public directory to match the new format and maintain consistency across both implementations.
    
    These changes aim to enhance SEO and provide clearer instructions for web crawlers accessing the site.

--- app/routes/robots[.]txt.tsx
+++ app/routes/robots[.]txt.tsx
@@ -1,14 +1,12 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 
 export function loader({ request }: LoaderFunctionArgs) {
-  const robotText = `
-# Allow all crawlers
-User-agent: *
+  const robotText = `User-agent: *
 Allow: /
+Disallow:
 
-# Sitemap location (uncomment and update when you have a sitemap)
-Sitemap: https://memoryview.in/sitemap.xml
-`;
+# Sitemap location
+Sitemap: https://memoryview.in/sitemap.xml`;
 
   return new Response(robotText, {
     status: 200,
